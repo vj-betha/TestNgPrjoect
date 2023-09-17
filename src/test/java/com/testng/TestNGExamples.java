559260f727417526d4b3b5ad3ba83279e3d354da
vijay_betha <vj.betha@gmail.com>
2023-09-17 11:33:26 -0400

    Adding the TestNG base project

---
+++ src/test/java/com/testng/TestNGExamples.java
@@ -0,0 +1,12 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+
+public class TestNGExamples {
+	
+	@Test
+	public void run1st() {
+	System.out.println("this is a sample test");	
+	}
+
+}
